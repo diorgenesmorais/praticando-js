d6d14ba5211fcd4ee6d7346e61c5baf0e13b30bd
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-04-13 13:04:37 -0300

    5.1b exercise - Rest

--- tests/fifthExercise.spec.js
+++ tests/fifthExercise.spec.js
@@ -1,10 +1,20 @@
 const expect = require('chai').expect;
 
 describe('Fifth exercise', () => {
-    it('5.1 Rest', () => {
-        const arr = [1, 2, 3, 4, 5, 6];
-        [ x, ...y ] = arr;
-        expect(x).to.equals(1);
-        expect(y).to.include.members([2, 3, 4, 5, 6]);
+    context('5.1 exercise - Rest', () => {
+        it('Should receive everything else', () => {
+            const arr = [1, 2, 3, 4, 5, 6];
+            [ x, ...y ] = arr;
+            expect(x).to.equals(1);
+            expect(y).to.include.members([2, 3, 4, 5, 6]);
+        });
+    
+        it('Should add all parameters', () => {
+            function somar(...args) {
+                return args.reduce((total, value) => total + value);
+            }
+            expect(somar(1,2,3,4,5,6)).to.equals(21);
+            expect(somar(1,2)).to.equals(3);
+        });
     });
 });
