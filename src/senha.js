06e6b024541b764ad16fb7eac23dee6373982c2d
Diorgenes Morais <diorgenesmorais@gmail.com>
2020-05-10 19:18:57 -0300

    deve ser alfanumerico

--- src/senha.js
+++ src/senha.js
@@ -1,9 +1,24 @@
 "use strict";
 const senha = function() {
+    const MIN_PASSWORD_LEN = 4;
+    const MAX_PASSWORD_LEN = 19;
+    
     return {
-        verify: function(password) {
-            return password.length > 3 && password.length < 20;
+        validator: function(password) {
+            if (!validatePasswordLength(password))
+                return false;
+            if (!isAlphanumeric(password))
+                return false;
+            return true;
         }
+    }
+
+    function validatePasswordLength(password) {
+        return (password.length >= MIN_PASSWORD_LEN && password.length <= MAX_PASSWORD_LEN);
+    }
+    function isAlphanumeric(word) {
+        const rex = /^[a-z0-9]+$/i;
+        return rex.test(word);
     }
 }
 
--- tests/senha.spec.js
+++ tests/senha.spec.js
@@ -2,8 +2,12 @@ const expect = require('chai').expect;
 const senha = require('../src/senha');
 
 describe('Desafio verificador de senha', () => {
+    const pass = senha();
     it('Should more than 3 characters but than 20', () => {
-        const pass = senha();
-        expect(pass.verify('1234')).to.true;
+        expect(pass.validator('DuckMoreVeryCharacters')).to.false;
+        expect(pass.validator('123')).to.false;
     });
+    it('Should by alphanumeric', () => {
+        expect(pass.validator('l.Dwieir!')).to.false;
+    })
 });
